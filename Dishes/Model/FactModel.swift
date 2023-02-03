b9ac8ef72b1a1ac2ab35759fb0ea39c22d133fdc
isveckrali <mehmet.can.seyhan1@gmail.com>
2023-02-03 22:50:18 +0100

    fact view added

---
+++ Dishes/Model/FactModel.swift
@@ -0,0 +1,14 @@
+//
+//  FactModel.swift
+//  Dishes
+//
+//  Created by Mehmet Seyhan on 3.02.2023.
+//
+
+import SwiftUI
+
+// MARK: - FACT MODEL
+
+struct Fact: Identifiable{
+    var id = UUID()
+}
--- Dishes/View/FactsView.swift
+++ Dishes/View/FactsView.swift
@@ -12,7 +12,40 @@ struct FactsView: View {
     
     // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Text("Even though the Swedes have some quite strange food to offer, there are some dishes you just have to give a chance. Check out our guide featuring ten of the best below!")
+                .padding(.leading, 55)
+                .padding(.trailing, 10)
+                .padding(.vertical, 3)
+                .frame(width: 300, height: 135, alignment: .center)
+                .background(LinearGradient(gradient: Gradient(colors: [Color("ColorYellowMedium"), Color("ColorYellowLight")]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(12)
+                .lineLimit(6)
+                .multilineTextAlignment(.leading)
+                .font(.footnote)
+                .foregroundColor(.white)
+            
+            Image("dish-fact-2")
+                .resizable()
+                .frame(width: 66, height: 66, alignment: .center)
+                .clipShape(Circle())
+                .background(
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 74, height: 74, alignment: .center)
+                )
+                .background(
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color("ColorYellowMedium"), Color("ColorYellowLight")]), startPoint: .trailing, endPoint: .leading))
+                        .frame(width: 82, height: 82, alignment: .center)
+                )
+                .background(
+                    Circle()
+                        .fill(Color("ColorApperanceAdaptive"))
+                        .frame(width: 90, height: 90, alignment: .center)
+                )
+                .offset(x: -150)
+        }
     }
 }
 
@@ -20,5 +53,6 @@ struct FactsView: View {
 struct FactsView_Previews: PreviewProvider {
     static var previews: some View {
         FactsView()
+            .previewLayout(.fixed(width: 400, height: 220))
     }
 }
